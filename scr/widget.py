744227da46fa97437c8253db76633df1fd2119d9
Reechka <R2echka@gmail.com>
2024-04-25 11:17:02 +0300

    fully cleared .git

---
+++ scr/widget.py
@@ -0,0 +1,20 @@
+from datetime import datetime as dt
+from scr.masks_ import card_mask, bill_mask
+
+
+def mask(info: str) -> str:
+    """Соединение функций масок в одну"""
+    if "Счет" in info:
+        return f"Счет {bill_mask(info.split()[-1])}"
+    else:
+        card_info = info.split()
+        return str(" ".join(card_info[:-1]) + " " + card_mask(card_info[-1]))
+
+
+def date_correction(date: str) -> str:
+    """Форматирует дату"""
+    formated_date = dt.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+    return formated_date.strftime("%d.%m.%Y")
+
+
+print(date_correction("2018-07-11T02:26:18.671407"))
