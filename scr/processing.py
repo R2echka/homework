de0a67f19255241bfaf4f7e7d9551e8884476972
Reechka <R2echka@gmail.com>
2024-04-28 18:04:38 +0300

    added new func and some changes

--- scr/processing.py
+++ scr/processing.py
@@ -1,11 +1,16 @@
-import sys, os
-sys.path.append(os.getcwd())
-from scr.widget import date_correction
-
-
-def sort_by_state(list_: list, state_status:str = 'executed') -> str:
+def sort_by_state(list_: list, state_status: str = "executed") -> list:
+    '''Получает список и возвращает список словарей, удовлетворяющих заданному значению "state"'''
     sorted_list = []
     for dict_ in list_:
-        if dict['state'] == state_status.upper():
+        if dict_["state"] == state_status.upper():
             sorted_list.append(dict_)
+    return sorted_list
+
+
+def date_sort(list_: list, time: str = "increasing") -> list:
+    sorted_list = []
+    if time == "increasing":
+        sorted_list = sorted(list_, key=lambda dict_: dict_["date"], reverse=True)
+    elif time == "decreasing":
+        sorted_list = sorted(list_, key=lambda dict_: dict_["date"])
     return sorted_list
