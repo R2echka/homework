0db85d82e67d8c50bb2ab9165d130b69a424c92f
Reechka <R2echka@gmail.com>
2024-05-07 21:26:24 +0300

    linter fixes

--- scr/decorators.py
+++ scr/decorators.py
@@ -1,26 +1,25 @@
-from typing import Optional, Callable, Any
+from functools import wraps
+from typing import Any, Callable, Optional
 
-def log(filename: Optional[str]=None) -> Callable:
+
+def log(filename: Optional[str] = None) -> Callable:
+    """Записывает в файл/ выводит в консоль результат выполнения декорируемой функции"""
+
     def dec(func: Callable) -> Callable:
+        @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> str:
             try:
                 func(*args, **kwargs)
-                result = f'{func.__name__} ok'
+                result = f"{func.__name__} ok"
             except Exception as e:
-                result = f'{func.__name__} error: {type(e).__name__}. Inputs: {(args)}, {kwargs}'
+                result = f"{func.__name__} error: {type(e).__name__}. Inputs: {(args)}, {kwargs}"
             if filename:
-                with open(filename, 'a') as f:
-                    f.write(result, '\n')
+                with open(filename, "a") as f:
+                    f.write(result + "\n")
             else:
                 print(result)
             return result
+
         return wrapper
+
     return dec
-
-@log()
-def idk(first, sec):
-    return first/sec
-
-a = idk(4, 2)
-b = idk(0, 5)
-c = idk(1, 0)
