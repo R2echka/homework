8457d0c848d81ff07b573fa48bcb8b4e5a8d8e18
Reechka <R2echka@gmail.com>
2024-05-14 12:33:02 +0300

    new functions

--- scr/utils.py
+++ scr/utils.py
@@ -1,5 +1,10 @@
 import json
+import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+api_key = os.getenv('API_KEY')
 
 def read_json(filename: str) -> list:
     try:
@@ -8,3 +13,31 @@ def read_json(filename: str) -> list:
             return data
     except (FileNotFoundError, json.JSONDecodeError):
         return []
+
+def transaction_sum(transaction: dict) -> float:
+    response = requests.get('https://www.cbr-xml-daily.ru/daily_json.js', headers={"apikey": api_key}, timeout=3)
+    data = response.json()
+    if transaction['operationAmount']["currency"]['code'] == 'RUB':
+        amount = float(transaction['operationAmount']['amount'])
+        return amount
+    else:
+        valute = transaction['operationAmount']["currency"]['code']
+        currency = data['Valute'][valute]['Value']
+        amount = float(transaction['operationAmount']['amount'])
+        return amount * currency
+
+print(transaction_sum({
+    "id": 41428829,
+    "state": "EXECUTED",
+    "date": "2019-07-03T18:35:29.512364",
+    "operationAmount": {
+    "amount": "8221.37",
+    "currency": {
+        "name": "USD",
+        "code": "USD"
+    }
+    },
+    "description": "Перевод организации",
+    "from": "MasterCard 7158300734726758",
+    "to": "Счет 35383033474447895560"
+}))
