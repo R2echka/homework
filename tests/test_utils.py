39eaf23d3bdef2ed67ec13fd708bec61e533ea4f
Reechka <R2echka@gmail.com>
2024-05-16 13:12:13 +0300

    lint fixes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,20 +1,30 @@
-import pytest
-import sys, os
-sys.path.append(os.getcwd())
+# import os
+# import sys
+# sys.path.append(os.getcwd())
 from unittest.mock import patch
+
+import pytest
 
 from scr.utils import read_json, transaction_sum
 
+
 @pytest.fixture
 def first_operation() -> dict:
-    return{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364', 'operationAmount': {'amount': '8221.37', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'MasterCard 7158300734726758', 'to': 'Счет 35383033474447895560'}
+    return {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560",
+    }
 
 
-def test_read_json(first_operation) -> None:
+def test_read_json(first_operation: list) -> None:
     "Тест чтения json-файла"
-    assert read_json('idk.json') == []
-    assert read_json('operations.json')[1] == first_operation
-
+    assert read_json("idk.json") == []
+    assert read_json("operations.json")[1] == first_operation
 
 
 def test_transaction_sum() -> None:
