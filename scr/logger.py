982ccc173d58464c7dee8b42d469e66b1bc5cd2f
Reechka <R2echka@gmail.com>
2024-05-21 06:49:40 +0300

    added logger to masks

--- scr/logger.py
+++ scr/logger.py
@@ -2,10 +2,11 @@ import logging as log
 from logging import Logger
 
 
-def logger() -> Logger:
+def log_setup() -> Logger:
+    '''Настройка логера'''
     logger = log.getLogger(__name__)
     logger.setLevel(log.DEBUG)
-    file_handler = log.FileHandler("code.log", "w")
+    file_handler = log.FileHandler("code.log", "w", encoding='utf-8')
     file_handler.setFormatter(log.Formatter("%(asctime)s - %(module)s, %(levelname)s: %(message)s"))
     logger.addHandler(file_handler)
     return logger
--- scr/masks.py
+++ scr/masks.py
@@ -1,12 +1,30 @@
+import sys, os
+sys.path.append(os.getcwd())
+from scr.logger import log_setup
+
+logger = log_setup()
+
 def card_mask(card_num: str) -> str:
     """Принимаает номер карты и возвращает его маску"""
-    card_num = str(card_num)
-    mask = " ".join([card_num[:4], card_num[4:6] + "**", "****", card_num[-4:]])
-    return mask
+    if len(card_num) == 16:
+        card_num = str(card_num)
+        mask = " ".join([card_num[:4], card_num[4:6] + "**", "****", card_num[-4:]])
+        logger.info('Применена функция card_mask')
+        return mask
+    else:
+        logger.error('Некорректно введён номер карты')
+        return None
 
 
 def bill_mask(bill_num: str) -> str:
     """Принимает номер счёта и возвращет его маску"""
-    bill_num = str(bill_num)
-    mask = "**" + bill_num[-4:]
-    return mask
+    if len(bill_num) == 21:
+        bill_num = str(bill_num)
+        mask = "**" + bill_num[-4:]
+        logger.info('Применена функция bill_mask')
+        return mask
+    else:
+        logger.error('Некорректно введён номер счёта')
+        return None
+
+
